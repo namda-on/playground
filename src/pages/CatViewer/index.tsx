63936bee07a34b6ac8c3d86eae1486fd73923c67
Jinyong Park <jinyoung1997@gmail.com>
2024-07-30 23:06:27 +0900

    feat : cat list 무한 스크롤 추가
    interscetion observer api 활용

--- src/pages/CatViewer/index.tsx
+++ src/pages/CatViewer/index.tsx
@@ -1,7 +1,7 @@
 import CatListColumn from "./components/CatListColumn";
 import ColumnCalculator from "./components/ColumnCalculator";
 import { useCatsByColumns, useCatStoreActions } from "@/store/cat";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import axios from "axios";
 import { END_POINT } from "@/constants/endpoints";
 import { Cat } from "@/types/cat";
@@ -11,19 +11,43 @@ import "./cat.css";
 function CatViewer() {
   const catsByColumn = useCatsByColumns();
   const { addCats } = useCatStoreActions();
+  const scrollEndDivRef = useRef<HTMLDivElement>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(0);
 
   useLayoutEffect(
     function fetchCat() {
       (async function fetchCatApi() {
+        setIsLoading(true);
         const result = await axios.get<Cat[]>(
-          `${END_POINT.CAT_API}/images/search?limit=30`,
+          `${END_POINT.CAT_API}/images/search?limit=30&page=${page}`,
           { headers: { "x-api-key": process.env.REACT_APP_CAT_API_KEY } }
         );
+        setIsLoading(false);
         addCats(result.data);
       })();
     },
-    [addCats]
+    [addCats, page]
   );
+
+  useLayoutEffect(function registerIntersectionObserver() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !isLoading) {
+            setPage((prev) => prev + 1);
+          }
+        });
+      },
+      { rootMargin: "800px" }
+    );
+
+    if (scrollEndDivRef.current) {
+      observer.observe(scrollEndDivRef.current);
+    }
+
+    return () => observer.disconnect();
+  });
 
   return (
     <ColumnCalculator>
@@ -33,6 +57,10 @@ function CatViewer() {
             <CatListColumn key={columnIdx} cats={catList} />
           ))}
         </div>
+        <div
+          ref={scrollEndDivRef}
+          style={{ width: "100%", height: "1px", color: "red" }}
+        />
       </main>
     </ColumnCalculator>
   );
