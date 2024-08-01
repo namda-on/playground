1e3a938fe1bedb8fac839914f21a9622ddb8564c
Jinyong Park <jinyoung1997@gmail.com>
2024-08-02 01:20:59 +0900

    feat : card click시 확대 기능추가

--- src/pages/CatViewer/components/CatCard.tsx
+++ src/pages/CatViewer/components/CatCard.tsx
@@ -1,20 +1,42 @@
-import { memo } from "react";
+import { memo, useCallback, useRef } from "react";
 import { Cat } from "@/types/cat";
 
 interface CatCardProps {
   cat: Cat;
 }
 
+const EXPANDED_CLASS = "card-expanded";
+
 function CatCard({ cat }: CatCardProps) {
+  const cardRef = useRef<HTMLDivElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  const onClickCard = useCallback(() => {
+    const cardElement = cardRef.current;
+    if (!cardElement) return;
+    if (cardElement.classList.contains(EXPANDED_CLASS)) {
+      cardElement.style.removeProperty("height");
+      cardElement.classList.remove(EXPANDED_CLASS);
+    } else {
+      const { height } = cardElement.getBoundingClientRect();
+      cardElement.style.height = `${height}px`;
+
+      cardElement.classList.add(EXPANDED_CLASS);
+    }
+  }, []);
+
   return (
-    <div className="card">
-      <img
-        className="card"
-        width={cat.width}
-        height={cat.height}
-        src={cat.url}
-        alt={`cat${cat.id}`}
-      />
+    <div ref={cardRef} className="card" onClick={onClickCard}>
+      <div className="image-container">
+        <img
+          ref={imgRef}
+          className="card"
+          width={cat.width}
+          height={cat.height}
+          src={cat.url}
+          alt={`cat${cat.id}`}
+        />
+      </div>
     </div>
   );
 }
